e63283a6c194b8e5bbd0f0a8a945ae2fde0ba1b0
Nikolaos Dimopoulos <nikos@niden.net>
2019-05-28 20:06:14 -0400

    Added assets and fixtures

---
+++ tests/_data/fixtures/Http/Message/StreamFixture.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Cardoe Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Cardoe\Test\Fixtures\Http\Message;
+
+use Cardoe\Http\Message\Stream;
+
+class StreamFixture extends Stream
+{
+    public function setHandle($handle)
+    {
+        $this->handle = $handle;
+    }
+
+    public function isSeekable(): bool
+    {
+        return false;
+    }
+
+    public function isWritable(): bool
+    {
+        return false;
+    }
+}
