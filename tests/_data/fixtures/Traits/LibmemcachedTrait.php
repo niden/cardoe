1f79461a47aecd3e8bb67f6da9ea950281d365ac
Nikolaos Dimopoulos <nikos@niden.net>
2019-06-17 12:09:52 -0400

    Adjutments to the composer; Added fixtures

---
+++ tests/_data/fixtures/Traits/LibmemcachedTrait.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Cardoe Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Cardoe\Test\Fixtures\Traits;
+
+use UnitTester;
+
+trait LibmemcachedTrait
+{
+    public function _before(UnitTester $I)
+    {
+        $I->checkExtensionIsLoaded('memcached');
+    }
+}
---
+++ tests/_data/fixtures/Traits/RedisTrait.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Cardoe Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Cardoe\Test\Fixtures\Traits;
+
+use UnitTester;
+
+trait RedisTrait
+{
+    protected $options = [];
+
+    public function _before(UnitTester $I)
+    {
+        $I->checkExtensionIsLoaded('redis');
+ 
+        $this->options = $this->getOptions();
+    }
+
+    protected function getOptions(): array
+    {
+        return [
+            'host'     => env('DATA_REDIS_HOST'),
+            'port'     => env('DATA_REDIS_PORT'),
+            'index'    => env('DATA_REDIS_NAME'),
+            'statsKey' => '_PHCR',
+        ];
+    }
+}
