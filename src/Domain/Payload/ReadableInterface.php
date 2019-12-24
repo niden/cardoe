23bb6e3cbc575ea30d460ea7c27ebbe5a90d84b6
Nikolaos Dimopoulos <nikos@niden.net>
2019-12-24 18:17:28 -0500

    Corrected docblocks

--- src/Domain/Payload/ReadableInterface.php
+++ src/Domain/Payload/ReadableInterface.php
@@ -3,17 +3,18 @@
 /**
  * This file is part of the Phalcon Framework.
  *
- * For the full copyright and license information, please view the LICENSE.md
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
  * file that was distributed with this source code.
  *
- * Implementation of this file has been influenced by cardoe-api and AuraPHP
- *
- * @link    https://github.com/cardoe/cardoe-api
- * @license https://github.com/cardoe/cardoe-api/blob/master/LICENSE
+ * Implementation of this file has been influenced by phalcon-api and AuraPHP
+ * @link    https://github.com/phalcon/rest-api
+ * @license https://github.com/phalcon/rest-api/blob/master/LICENSE
  * @link    https://github.com/auraphp/Aura.Payload
  * @license https://github.com/auraphp/Aura.Payload/blob/3.x/LICENSE
  *
- * @see     Original inspiration for the https://github.com/cardoe/cardoe-api
+ * @see Original inspiration for the https://github.com/phalcon/rest-api
  */
 
 declare(strict_types=1);
