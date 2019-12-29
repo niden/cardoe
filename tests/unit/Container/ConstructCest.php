086aeb8caee05bde009e71885b5981ee71e15690
Nikolaos Dimopoulos <nikos@niden.net>
2019-12-29 11:31:29 -0500

    Added test stubs

---
+++ tests/unit/Container/ConstructCest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Test\Unit\Container;
+
+use UnitTester;
+
+class ConstructCest
+{
+    /**
+     * Unit Tests Phalcon\Container :: __construct()
+     *
+     * @since  2019-12-28
+     */
+    public function containerConstruct(UnitTester $I)
+    {
+        $I->wantToTest('Container - __construct()');
+
+        $I->skipTest('Need implementation');
+    }
+}
---
+++ tests/unit/Container/GetCest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Test\Unit\Container;
+
+use UnitTester;
+
+class GetCest
+{
+    /**
+     * Unit Tests Phalcon\Container :: get()
+     *
+     * @since  2019-12-28
+     */
+    public function containerGet(UnitTester $I)
+    {
+        $I->wantToTest('Container - get()');
+
+        $I->skipTest('Need implementation');
+    }
+}
