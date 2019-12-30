6e385f92e48e94356216684ed7563457546b2678
Nikolaos Dimopoulos <nikos@niden.net>
2019-12-30 01:55:03 -0500

    Added missing interface

---
+++ src/Container/Injection/MutationInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ *
+ * Implementation of this file has been influenced by AuraPHP
+ *
+ * @link    https://github.com/auraphp/Aura.Di
+ * @license https://github.com/auraphp/Aura.Di/blob/4.x/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Container\Injection;
+
+/**
+ * Indicates a Mutation to be invoked after constructing an object
+ */
+interface MutationInterface
+{
+    /**
+     * Invokes the Mutation to return an object.
+     *
+     * @param object $object
+     *
+     * @return object
+     */
+    public function __invoke(object $object): object;
+}
