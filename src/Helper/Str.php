c8617f949083dabbba332a10f8a8a2d6bc09f261
Nikolaos Dimopoulos <nikos@niden.net>
2020-03-11 11:29:15 -0400

    [PSR] - Correction to friendly

--- src/Helper/Str.php
+++ src/Helper/Str.php
@@ -13,7 +13,9 @@ declare(strict_types=1);
 
 namespace Phalcon\Helper;
 
+use function array_keys;
 use function array_merge;
+use function array_values;
 use function count_chars;
 use function explode;
 use function func_get_args;
@@ -287,6 +289,28 @@ class Str
         bool $lowercase = true,
         $replace = null
     ): string {
+        $matrix = [
+            "Š"    => "S",     "š"    => "s", "Đ"    => "Dj", "Ð"    => "Dj",
+            "đ"    => "dj",    "Ž"    => "Z", "ž"    => "z",  "Č"    => "C",
+            "č"    => "c",     "Ć"    => "C", "ć"    => "c",  "À"    => "A",
+            "Á"    => "A",     "Â"    => "A", "Ã"    => "A",  "Ä"    => "A",
+            "Å"    => "A",     "Æ"    => "A", "Ç"    => "C",  "È"    => "E",
+            "É"    => "E",     "Ê"    => "E", "Ë"    => "E",  "Ì"    => "I",
+            "Í"    => "I",     "Î"    => "I", "Ï"    => "I",  "Ñ"    => "N",
+            "Ò"    => "O",     "Ó"    => "O", "Ô"    => "O",  "Õ"    => "O",
+            "Ö"    => "O",     "Ø"    => "O", "Ù"    => "U",  "Ú"    => "U",
+            "Û"    => "U",     "Ü"    => "U", "Ý"    => "Y",  "Þ"    => "B",
+            "ß"    => "Ss",    "à"    => "a", "á"    => "a",  "â"    => "a",
+            "ã"    => "a",     "ä"    => "a", "å"    => "a",  "æ"    => "a",
+            "ç"    => "c",     "è"    => "e", "é"    => "e",  "ê"    => "e",
+            "ë"    => "e",     "ì"    => "i", "í"    => "i",  "î"    => "i",
+            "ï"    => "i",     "ð"    => "o", "ñ"    => "n",  "ò"    => "o",
+            "ó"    => "o",     "ô"    => "o", "õ"    => "o",  "ö"    => "o",
+            "ø"    => "o",     "ù"    => "u", "ú"    => "u",  "û"    => "u",
+            "ý"    => "y",     "þ"    => "b",  "ÿ"   => "y",  "Ŕ"    => "R",
+            "ŕ"    => "r",     "ē"    => "e",  "'"   => "",
+            "&"    => " and ", "\r\n" => " ", "\n"   => " "
+        ];
 
         if (null !== $replace) {
             if (!is_array($replace) && !is_string($replace)) {
@@ -299,9 +323,12 @@ class Str
                 $replace = [$replace];
             }
 
-            $text = str_replace($replace, " ", $text);
+            foreach ($replace as $item) {
+                $matrix[(string) $item] = " ";
+            }
         }
 
+        $text     = str_replace(array_keys($matrix), array_values($matrix), $text);
         $friendly = preg_replace(
             "/[^a-zA-Z0-9\\/_|+ -]/",
             "",
